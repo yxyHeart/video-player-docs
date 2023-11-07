3269feab0d2db4ea63869f96c7dd20476fe265de
yxyHeart <56054482+yxyHeart@users.noreply.github.com>
2023-11-07 20:42:48 +0800

    基本完成框架

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,15 +4,19 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/yxyheart/video-player-docs',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/yxyheart/video-player-docs',
   footer: {
     text: 'Nextra Docs Template',
   },
+  sidebar:{
+    "defaultMenuCollapseLevel":10
+  }
+
 }
 
 export default config
